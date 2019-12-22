dda15bd98732fc8a87e5ce73399d07d2e1489cd1
sanketmunot <47555024+sanketmunot@users.noreply.github.com>
2019-12-22 19:16:06 +0530

    Add files via upload
    Output as CSV format

--- script.js
+++ script.js
@@ -4,7 +4,7 @@ var si_ze
 function details(){
 	clas = document.getElementById('clas').value
 	size = document.getElementById('size').value
-	window.open("https://sanketmunot.000webhostapp.com/presentee/mark.html?size="+size+"&clas="+clas)
+	window.open("mark.html?size="+size+"&clas="+clas)
 }
 
 $(document).ready(function(){
@@ -28,44 +28,64 @@ function dosom(e){
 	if(e.target !== e.currentTarget){
 		var item = e.target.id;
 		if (document.getElementById(item).style.backgroundColor == "green"){
+			document.getElementById(item).style.backgroundColor = "yellow";
+		}
+		else if(document.getElementById(item).style.backgroundColor == "yellow"){
 			document.getElementById(item).style.backgroundColor = "red";
 		}
-		else {
+		else{
 			document.getElementById(item).style.backgroundColor = "green";
 		}
 	}
 	e.stopPropogation();
 }
 
-function submitpresent(){
+
+
+function submit(){
 	
-	arr= [];
+	arr_present=[];
+	arr_absent=[];
+	arr_late=[];
 	for (var i=1;i<=si_ze;i++){
 		if (document.getElementById('box'+i).style.backgroundColor == "green"){
-			arr.push(i);
+			arr_present.push(i);
 		}
-	}
-	//alert(arr);
-	var today = new Date();
-    today = today.toISOString().substring(0, 10);
-	url = 'https://api.whatsapp.com/send?text=%20' +"Date : "+today+" "+cl_as+" Present Numbers : "+arr;
-	window.open(url);
-	window.open("index.html","_self")
-}
+		else if (document.getElementById('box'+i).style.backgroundColor == "yellow"){
+			arr_late.push(i);
+		}
+		else{
+			arr_absent.push(i);
+		}
 
-function submitabsent(){
-	
-	arr=[];
-	for (var i=1;i<=si_ze;i++){
-		if (document.getElementById('box'+i).style.backgroundColor != "green"){
-			arr.push(i);
-		}
 	}
-	//alert(arr);
+	
 	var today = new Date();
-    today = today.toISOString().substring(0, 10);
-	url = 'https://api.whatsapp.com/send?text=%20' +"Date : "+today+" "+cl_as+" Absent Numbers : "+arr;
-	window.open(url);
+	today = today.toISOString().substring(0, 10);
+	var csv = 'Present,'
+	for (var i=0;i<=arr_present.length-1;i++){
+		csv = csv+arr_present[i]
+		csv = csv+","
+	}
+	csv = csv+"\n"
+	csv = csv+"Absent,"
+	for (var i=0;i<=arr_absent.length-1;i++){
+		csv = csv+arr_absent[i]
+		csv = csv+","
+	}
+	csv = csv+"\n"
+	csv = csv+"Late,"
+	for (var i=0;i<=arr_late.length-1;i++){
+		csv = csv+arr_late[i]
+		csv = csv+","
+	}
+	alert(csv);
+	var hiddenElement = document.createElement('a');
+	hiddenElement.href = 'data:text/csv;charset=utf-8,' + encodeURI(csv);
+	hiddenElement.download = cl_as+'-'+today+'.csv';
+	hiddenElement.click();
+	
+
 }
 
 function absentall(){
